831c7e1dc43adb6bfcec62db1aaef3a1bf5e2ac8
alexadler9 <alexadler@bk.ru>
2024-03-13 23:48:30 +0500

    Added processing of new articles in Worker

--- app/src/main/java/ru/alexadler9/newsfetcher/feature/newsworker/NewsPollWorker.kt
+++ app/src/main/java/ru/alexadler9/newsfetcher/feature/newsworker/NewsPollWorker.kt
@@ -1,5 +1,6 @@
 package ru.alexadler9.newsfetcher.feature.newsworker
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.Log
 import androidx.hilt.work.HiltWorker
@@ -13,13 +14,31 @@ private const val TAG = "NEWS_POLL_WORKER"
 
 @HiltWorker
 class NewsPollWorker @AssistedInject constructor(
-    @Assisted context: Context,
+    @Assisted val context: Context,
     @Assisted workerParams: WorkerParameters,
     private val repository: NewsRepository
 ) : CoroutineWorker(context, workerParams) {
 
+    @SuppressLint("UnspecifiedImmutableFlag")
     override suspend fun doWork(): Result {
-        Log.d(TAG, "doWork()")
+        val articles = repository.getTopHeadlinesArticles(
+            country = repository.getArticlesCountry(),
+            category = repository.getArticlesCategory(),
+            query = ""
+        )
+
+        if (articles.isEmpty()) {
+            return Result.success()
+        }
+
+        val articleUrl = articles.first().url
+        if (articleUrl == repository.getLastArticleUrl()) {
+            Log.i(TAG, "Got an old URL: $articleUrl")
+        } else {
+            Log.i(TAG, "Got a new URL: $articleUrl")
+            repository.saveLastArticleUrl(articleUrl)
+        }
+
         return Result.success()
     }
 }
